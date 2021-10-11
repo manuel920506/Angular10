0557c6c79964701a57cc7cc1054ed694d8825900
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2021-10-11 04:20:23 +0200

    aggiunte catelle tre componenti nuovi, loro stile e route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LoginComponent } from './componets/start/login/login.component';
+import { RegisterComponent } from './componets/start/register/register.component';
+import { WelcomeComponent } from './componets/start/welcome/welcome.component';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/welcome', pathMatch: 'full' } , 
+  { path: 'welcome', component: WelcomeComponent } , 
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  {path: '**', redirectTo: '/welcome', pathMatch: 'full' } 
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-
+import { StartComponent } from './componets/start/start.component';
+import { WelcomeComponent } from './componets/start/welcome/welcome.component';
+import { LoginComponent } from './componets/start/login/login.component';
+import { RegisterComponent } from './componets/start/register/register.component';
+  
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StartComponent,
+    WelcomeComponent,
+    LoginComponent,
+    RegisterComponent 
   ],
   imports: [
     BrowserModule,
