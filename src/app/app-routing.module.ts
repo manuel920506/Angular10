8ee89511354d0e5aeafa0cb39aaeb742373bf5c7
Manuel Rodriguez <manuel.rodriguez15@libero.it>
2021-10-17 11:30:56 +0200

    ristrutturazione albero routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,18 @@ import { RegisterComponent } from './components/start/register/register.componen
 import { WelcomeComponent } from './components/start/welcome/welcome.component';
 import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { StartComponent } from './components/start/start.component';
 
-const routes: Routes = [
-  { path: '', redirectTo: '/welcome', pathMatch: 'full' } , 
-  { path: 'welcome', component: WelcomeComponent } , 
-  { path: 'register', component: RegisterComponent },
-  { path: 'login', component: LoginComponent },
+const routes: Routes = 
+[
+  { path: '', redirectTo: '/start', pathMatch: 'full' } , 
+  {path: 'start', component: StartComponent, children:
+    [
+      { path: '', component: WelcomeComponent } , 
+      { path: 'register', component: RegisterComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  }, 
   { path: '**', redirectTo: '/welcome', pathMatch: 'full' } 
 ];
 
