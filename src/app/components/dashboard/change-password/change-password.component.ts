a9f3fda8173a557112ab755ce840c15c17dce2d3
Manuel Rodriguez <mrodriguez@onit.it>
2021-11-06 05:33:47 +0100

    Connessione backend e Frontend, caso d'uso cambio password

--- src/app/components/dashboard/change-password/change-password.component.ts
+++ src/app/components/dashboard/change-password/change-password.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-change-password',
@@ -7,10 +10,15 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   styleUrls: ['./change-password.component.css']
 })
 export class ChangePasswordComponent implements OnInit {
-changePassword: FormGroup;
-  constructor(private fb: FormBuilder) {
+  changePassword: FormGroup;
+  loading: boolean = false;
+  constructor(private fb: FormBuilder,
+    private userService: UserService,
+    private toastr:ToastrService,
+    private router: Router
+    ){
     this.changePassword = this.fb.group({
-      lastPasword: ['', Validators.required],
+      lastPassword: ['', Validators.required],
       newPassword: ['', [Validators.required, Validators.minLength(4)]],
       confirmPassword: ['']
     }, {validators: this.checkPassword})
@@ -24,7 +32,24 @@ changePassword: FormGroup;
     const confirmPass  = group.controls.confirmPassword.value;
     return pass === confirmPass ? null : {notSame: true}
   } 
+
   SavePassword():void{
     console.log(this.changePassword);
-  }
+
+    const changePassword:any = {
+      lastPassword: this.changePassword.value.lastPassword,
+      newPassword: this.changePassword.value.newPassword
+    };
+    console.log(changePassword);
+    this.loading = true;
+    this.userService.changePassword(changePassword).subscribe(data => {
+      this.loading = false;
+      this.toastr.info(data.message);
+      this.router.navigate(['/dashboard']);
+    }, error => {
+    this.loading = false;
+    this.changePassword.reset();
+    this.toastr.error(error.error.message, 'Error!');
+    
+  })}; 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -10,16 +10,22 @@ import { Observable } from 'rxjs';
 export class UserService {
  myAppUrl:string;
  myApiUrl:string;
+ myCasoDUsoChangePassword:string;
 
   constructor(private http: HttpClient)
    { 
      this.myAppUrl = environment.endpoint;
      this.myApiUrl = '/api/User';
+     this.myCasoDUsoChangePassword = '/ChangePassword';
    }
 
   saveUser(user: User): Observable<any>
   {
     //https://localhost:44320/api/User  --Post
     return this.http.post(this.myAppUrl + this.myApiUrl, user);
+  }
+
+  changePassword(changePassword):Observable<any>{
+    return this.http.put(this.myAppUrl + this.myApiUrl + this.myCasoDUsoChangePassword, changePassword);
   }
 }
